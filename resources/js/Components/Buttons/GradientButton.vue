603302d9e0192da3c24ee16cab3e55ffde12ce27
Carl Nettelblad <carl.nettelblad@gmail.com>
2024-10-23 16:15:10 +0200

    feat: gradient button

---
+++ resources/js/Components/Buttons/GradientButton.vue
@@ -0,0 +1,7 @@
+<template>
+    <button
+        class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center"
+    >
+        <slot />
+    </button>
+</template>
