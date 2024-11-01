78d6ef3e38852bc5cb3205dbc1c2cf4e5fd4752b
Carl Nettelblad <carl.nettelblad@gmail.com>
2024-11-01 14:14:15 +0100

    chore: lint and update import form

--- resources/js/Pages/Import/Partials/ImportForm.vue
+++ resources/js/Pages/Import/Partials/ImportForm.vue
@@ -1,34 +1,31 @@
 <script setup lang="ts">
-import FileInput from '@/Components/Form/FileInput.vue';
-import InputError from '@/Components/Form/InputError.vue';
-import InputLabel from '@/Components/Form/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/Form/TextInput.vue';
-import Typeahead from '@/Components/Form/Typeahead.vue';
-import { useForm } from '@inertiajs/vue3';
-import { ref } from 'vue';
+import FileInput from "@/Components/Form/FileInput.vue";
+import InputError from "@/Components/Form/InputError.vue";
+import InputLabel from "@/Components/Form/InputLabel.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import { useForm } from "@inertiajs/vue3";
 
 const form = useForm({
     csv: null,
 });
 
 const importContestants = () => {
-    form.post(route('contestants.import'), {
+    form.post(route("contestants.import"), {
         preserveScroll: true,
         onSuccess: () => {
             form.reset();
         },
-        onError: () => {
-        },
+        onError: () => {},
     });
 };
-
 </script>
 
 <template>
     <section>
         <header>
-            <h2 class="text-lg font-medium text-gray-900">Import Contestants</h2>
+            <h2 class="text-lg font-medium text-gray-900">
+                Import Contestants
+            </h2>
 
             <p class="mt-1 text-sm text-gray-600">
                 Import contestants from a CSV file.
@@ -39,20 +36,19 @@ const importContestants = () => {
             <div>
                 <InputLabel for="csv" value="File" />
 
-                <FileInput
-                    id="csv"
-                    v-model="form.csv"
-                    accept=".csv"
-                    class="mt-1 block w-full"
-                />
+                <FileInput v-model="form.csv" accept=".csv" />
 
-                <p class="mt-1 text-sm text-gray-500" id="file_input_help">Accepted file formats: <strong>.csv</strong></p>
+                <p class="mt-1 text-sm text-gray-500" id="file_input_help">
+                    Accepted file formats: <strong>.csv</strong>
+                </p>
 
                 <InputError :message="form.errors.csv" class="mt-2" />
             </div>
 
             <div class="flex items-center gap-4">
-                <PrimaryButton :disabled="form.processing">Start import</PrimaryButton>
+                <PrimaryButton :disabled="form.processing">
+                    Start import
+                </PrimaryButton>
 
                 <Transition
                     enter-active-class="transition ease-in-out"
@@ -60,7 +56,12 @@ const importContestants = () => {
                     leave-active-class="transition ease-in-out"
                     leave-to-class="opacity-0"
                 >
-                    <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Import initiated</p>
+                    <p
+                        v-if="form.recentlySuccessful"
+                        class="text-sm text-gray-600"
+                    >
+                        Import initiated
+                    </p>
                 </Transition>
             </div>
         </form>
