ab715ee89a0d86bb32feb8f34c40b5cb9b27a1bc
Carl Nettelblad <carl.nettelblad@gmail.com>
2024-11-01 14:13:54 +0100

    feat: popover component

---
+++ resources/js/Components/Popover.vue
@@ -0,0 +1,9 @@
+<template>
+    <div
+        class="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm"
+    >
+        <div class="px-3 py-2">
+            <slot />
+        </div>
+    </div>
+</template>
