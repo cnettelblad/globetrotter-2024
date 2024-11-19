b11d408c8268918fd2d37fc0c8e1618073acb84e
Carl Nettelblad <1347549+cnettelblad@users.noreply.github.com>
2024-11-19 23:07:17 +0100

    refactor: psr-4 fixes

--- database/seeders/ImportFailureSeeder.php
+++ database/seeders/ImportFailureSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class ImportFailureSeederSeeder extends Seeder
+class ImportFailureSeeder extends Seeder
 {
     /**
      * Run the database seeds.
