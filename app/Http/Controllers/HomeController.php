921133cbdd560f3bd7701c3f4a945bdfdfd98fde
Carl Nettelblad <1347549+cnettelblad@users.noreply.github.com>
2025-01-15 22:58:34 +0100

    chore: create homecontroller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ContestantResource;
+use App\Models\Contestant;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class HomeController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        return Inertia::render('Home', [
+            'contestants' => ContestantResource::collection(
+                Contestant::query()
+                    ->with('submissions')
+                    ->withCount('submissions')
+                    ->orderBy('submissions_count', 'desc')
+                    ->get()
+            )
+        ]);
+    }
+}
