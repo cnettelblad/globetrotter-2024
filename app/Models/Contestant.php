904b246c4b941f2c181d7fe3048f7a1f411084a1
Carl Nettelblad <1347549+cnettelblad@users.noreply.github.com>
2025-01-15 23:01:45 +0100

    chore: remove unused imports

--- app/Models/Contestant.php
+++ app/Models/Contestant.php
@@ -8,8 +8,6 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Http\Client\RequestException;
-use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 
