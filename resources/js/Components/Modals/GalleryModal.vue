7a6a2b13365d3cb5f9fd0a46e629257bffe14eb0
Carl Nettelblad <1347549+cnettelblad@users.noreply.github.com>
2025-01-20 22:26:40 +0100

    fix: gallery image constraints

--- resources/js/Components/Modals/GalleryModal.vue
+++ resources/js/Components/Modals/GalleryModal.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { Submission } from "@/types";
 import { ChevronLeftIcon, ChevronRightIcon, XMarkIcon } from "@heroicons/vue/24/outline";
-import { onMounted, onUnmounted, ref } from "vue";
+import { computed, nextTick, onMounted, onUnmounted, ref } from "vue";
 
 const props = defineProps<{
     submissions: Submission[];
@@ -39,8 +39,20 @@ const nextImage = () => {
     currentImage.value = (currentImage.value + 1) % props.submissions.length;
 };
 
+const remainingHeight = ref("100vh"); // Default to full viewport height.
+
+const recalculateHeight = () => {
+    const header = document.getElementById("gallery-header")?.clientHeight || 0;
+    const footer = document.getElementById("gallery-footer")?.clientHeight || 0;
+    remainingHeight.value = `calc(100vh - ${header + footer}px)`;
+};
+
 onMounted(() => {
     document.addEventListener("keydown", handleKeyEvents);
+    // window.addEventListener("resize", recalculateHeight);
+
+    nextTick(() => recalculateHeight());
+
     document.body.style.overflow = "hidden";
 });
 onUnmounted(() => {
@@ -51,52 +63,54 @@ onUnmounted(() => {
 
 <template>
     <div
-        class="flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-screen max-h-screen"
+        class="flex flex-col overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-screen max-h-screen"
     >
-        <div
-            class="flex flex-col h-full max-h-screen items-center justify-center"
-        >
-
+        <div class="grid w-full" id="gallery-header">
             <button
-                class="absolute top-4 right-4 text-gray-200"
+                class="text-gray-200 place-self-end"
                 @click="cancel"
                 aria-label="Close"
             >
                 <XMarkIcon class="w-16 h-16" />
             </button>
-
+        </div>
+        <div
+            class="flex w-full h-full justify-between"
+        >
             <button
-                class="absolute left-0 top-1/2 transform -translate-y-1/2 text-white py-32 px-4 hover:bg-black/50"
+                class="text-white py-32 px-4 hover:bg-black/50"
                 @click="prevImage"
             >
                 <ChevronLeftIcon class="h-12 w-12" />
             </button>
 
-            <div class="flex p-8 max-h-full justify-center">
+            <div class="w-full h-full grid place-items-center">
                 <img
                     :src="props.submissions[currentImage].image"
                     :alt="props.submissions[currentImage].destination?.name"
-                    class="max-w-full max-h-[80vh]"
+                    class="max-w-full p-4"
+                    :style="{ maxHeight: remainingHeight }"
                 />
             </div>
 
             <button
-                class="absolute right-0 top-1/2 transform -translate-y-1/2 text-white py-32 px-4 hover:bg-black/50"
+                class="text-white py-32 px-4 hover:bg-black/50"
                 @click="nextImage"
             >
                 <ChevronRightIcon class="h-12 w-12" />
             </button>
-
-            <div class="flex gap-8 align-middle items-center overflow-hidden relative left-0 right-0">
+        </div>
+        <div id="gallery-footer">
+            <div class="flex gap-8 align-middle items-center overflow-hidden relative left-0 right-0 py-4 bg-gray-800/50">
                 <div
                     v-for="(submission, index) in props.submissions"
                     :key="index"
-                    class="p-1 bg-white min-w-16"
+                    class="w-16 h-16 rounded-[2rem] overflow-hidden hover:rounded-2xl transition-[border-radius] duration-250 ease-in-out cursor-pointer border-gray-800 border-4"
                 >
                     <img
                         :src="submission.image"
                         :alt="submission.destination?.name"
-                        class="w-16 max-h-full"
+                        class="object-cover w-full h-full scale-125"
                         @click="currentImage = index"
                     />
                 </div>
