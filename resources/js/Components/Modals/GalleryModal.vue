e31870de9274b31a72906a7dd5eeef51d1a0fef4
Carl Nettelblad <1347549+cnettelblad@users.noreply.github.com>
2025-01-23 23:37:06 +0100

    feat: display country and month in the gallery view

--- resources/js/Components/Modals/GalleryModal.vue
+++ resources/js/Components/Modals/GalleryModal.vue
@@ -2,6 +2,7 @@
 import { Submission } from "@/types";
 import { ChevronLeftIcon, ChevronRightIcon, XMarkIcon } from "@heroicons/vue/24/outline";
 import { computed, nextTick, onMounted, onUnmounted, ref } from "vue";
+import Emoji from "../Emoji.vue";
 
 const props = defineProps<{
     submissions: Submission[];
@@ -67,7 +68,12 @@ onUnmounted(() => {
         class="flex flex-col overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-screen max-h-screen"
     >
         <div class="grid grid-cols-6 w-full" id="gallery-header">
-            <h2 class="text-4xl font-bold col-start-2 bg-gray-800 col-span-4 text-white p-4 w-full text-center whitespace-nowrap overflow-hidden overflow-ellipsis">{{ title }}</h2>
+            <div class="col-start-2 bg-gray-800 col-span-4 text-white p-4 w-full text-center whitespace-nowrap overflow-hidden overflow-ellipsis">
+                <h2 class="text-4xl font-bold">{{ title }}</h2>
+                <span>{{ submissions[currentImage].destination?.name }}
+                    <Emoji>{{ submissions[currentImage].destination?.emoji }}</Emoji>
+                    in {{ submissions[currentImage].month }}</span>
+            </div>
             <button
                 class="text-gray-200 place-self-end"
                 @click="cancel"
